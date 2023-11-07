58656ce76f3aeb1b20e3ad78f1f2bd248e927e32
egbetayo <damilola.egbetayo@stanbicibtc.com>
2023-11-07 22:03:34 +0100

    feat: updated codebase and project structure

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,6 +1,6 @@
-export { default as exploreimg } from "./exploreimg.png";
-export { default as exploreimg2 } from "./exploreimg2.png";
-export { default as exploreimg3 } from "./exploreimg3.png";
+export { default as exploreimg } from "./exploreImg.png";
+export { default as exploreimg2 } from "./exploreImg.png";
+export { default as exploreimg3 } from "./exploreImg.png";
 export { default as introone } from "./introone.png";
 export { default as introsec } from "./introsec.png";
 export { default as loginimg } from "./login.png";
